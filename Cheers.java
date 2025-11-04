6b9fa9cf989254acf130a27b48fe29e864831328
YOAV-DORY <yoav.dory@post.runi.ac.il>
2025-11-04 20:09:08 +0200

    first commit

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,24 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String Word1 = args[0];
+            String Word = Word1.toUpperCase();
+            int Repeat = Integer.parseInt(args[1]);
+            int i = 0;
+            
+            while (i < Word.length()){
+                if (Word.indexOf("A") == i|| Word.indexOf("H") == i|| Word.indexOf("E") == i|| Word.indexOf("F") == i|| Word.indexOf("I") == i|| Word.indexOf("L") == i|| Word.indexOf("M") == i|| Word.indexOf("N") == i|| Word.indexOf("O") == i|| Word.indexOf("R") == i|| Word.indexOf("S") == i || Word.indexOf("X") == i){
+                        System.out.println("Give me an " + Word.charAt(i) + ": " + Word.charAt(i) + "!");
+                }
+                else{
+                        System.out.println("Give me a " + Word.charAt(i) + ": " + Word.charAt(i) + "!");
+                }
+                i++;
+            }
+            System.out.println("What does that spell?");
+
+            for (int j = 0; j < Repeat; j++){
+                System.out.println(Word + "!!!");
+            }
         }
 }
