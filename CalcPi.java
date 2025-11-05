daf71d4cf8973655116fab5c1792836a5d7f1e90
YOAV-DORY <yoav.dory@post.runi.ac.il>
2025-11-05 10:50:24 +0200

    first commit

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+		double ApproxPi = 1.0;
+		double Divider = 3;
+		while (Divider < N && Divider > -N) {
+			ApproxPi = ApproxPi -  (1/Divider);
+			if (Divider > 0){
+				Divider += 2;
+				Divider = -Divider;
+			}
+			else {
+				Divider = -Divider;
+				Divider += 2;
+			}
+			
+		}
+		System.out.println("pi according to java: " + Math.PI);
+		System.out.println("pi, approximated:     " + ApproxPi * 4);
 	}
 }
