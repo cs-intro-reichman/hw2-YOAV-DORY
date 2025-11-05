b2645dfcfd846c059e91780f5c44503e896c6f00
YOAV-DORY <yoav.dory@post.runi.ac.il>
2025-11-05 12:08:16 +0200

    final commit

--- Cheers.java
+++ Cheers.java
@@ -1,20 +1,15 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
 	    String Word1 = args[0];
             String Word = Word1.toUpperCase();
             int Repeat = Integer.parseInt(args[1]);
             int i = 0;
+            String letters = "AHEFILMNORSX";
             
             while (i < Word.length()){
-                if (Word.indexOf("A") == i|| Word.indexOf("H") == i|| Word.indexOf("E") == i|| Word.indexOf("F") == i|| Word.indexOf("I") == i|| Word.indexOf("L") == i|| Word.indexOf("M") == i|| Word.indexOf("N") == i|| Word.indexOf("O") == i|| Word.indexOf("R") == i|| Word.indexOf("S") == i || Word.indexOf("X") == i){
+                
+                if (letters.indexOf(Word.charAt(i)) != -1){
                         System.out.println("Give me an " + Word.charAt(i) + ": " + Word.charAt(i) + "!");
                 }
                 else{
