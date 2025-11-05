113c8b5f0d7367512318ac56dd12ccd6b019445f
YOAV-DORY <yoav.dory@post.runi.ac.il>
2025-11-05 22:46:39 +0200

    last commit

--- CalcPi.java
+++ CalcPi.java
@@ -5,13 +5,11 @@ public class CalcPi {
 		double ApproxPi = 1.0;
 		int Divider = 3;
 		int sign = -1;
-		
-		for (int i = 0 ; i < N ; i++) {
+		for (int i = 0 ; i < N-1 ; i++) {
 			ApproxPi += sign *  (1.0/Divider);
 			Divider += 2;
 			sign = -sign;
 		}
-		
 		System.out.println("pi according to Java: " + Math.PI);
 		System.out.println("pi, approximated:     " + ApproxPi * 4);
 	}
