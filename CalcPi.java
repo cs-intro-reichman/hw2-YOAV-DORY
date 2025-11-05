41b1750f86e2d4d9ed341cc024ddd4e08b7443b3
YOAV-DORY <yoav.dory@post.runi.ac.il>
2025-11-05 22:34:35 +0200

    Change

--- CalcPi.java
+++ CalcPi.java
@@ -3,19 +3,15 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int N = Integer.parseInt(args[0]);
 		double ApproxPi = 1.0;
-		double Divider = 3;
-		while (Divider <= N && Divider >= -N) {
-			ApproxPi = ApproxPi -  (1/Divider);
-			if (Divider > 0){
-				Divider += 2;
-				Divider = -Divider;
-			}
-			else {
-				Divider = -Divider;
-				Divider += 2;
-			}
-			
+		int Divider = 3;
+		int sign = -1;
+		
+		for (int i = 0 ; i < N ; i++) {
+			ApproxPi += sign *  (1.0/Divider);
+			Divider += 2;
+			sign = -sign;
 		}
+		
 		System.out.println("pi according to Java: " + Math.PI);
 		System.out.println("pi, approximated:     " + ApproxPi * 4);
 	}
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,39 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int c = 1;
+		int number = 0;
+		
+		if(mode.contains("v")){
+			while (c <= N) {
+				int runNum = 1;
+				if(c >= 2){
+					number = c;
+				}
+				while(number != 1){
+					if(number == 0){
+						number++;
+					}
+					System.out.print(number + " ");
+					if(number % 2 ==1){
+						number = number * 3 + 1;
+					}
+					else{
+						number = number / 2;
+					}
+					runNum += 1;
+				}
+				System.out.println(number + " (" + runNum + ")");
+				c++;
+			}
+			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+		}
+		else{
+			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+		}
+		
+		
 	}
 }
